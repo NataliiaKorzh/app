d76e72e87fcab46664a7be8e6bf3ac90894622f8
Nataliia <clarioruberia@gmail.com>
2024-05-29 09:51:23 +0300

    add app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"
+
+
+from models import User, Role, Group
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
